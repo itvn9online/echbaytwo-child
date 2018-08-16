66cd1f5d9c5bb218e183c347226bf727677cc7bf
itvn9online <itvn9online@gmail.com>
2018-08-17 02:40:03 +0700

    function for custom lang

--- functions.php
+++ functions.php
@@ -8,3 +8,18 @@
 */
 define( 'EB_CHILD_THEME_URL', dirname( __FILE__ ) . '/' );
 //echo 'EB_CHILD_THEME_URL: ' . EB_CHILD_THEME_URL . '<br>';
+
+
+
+// custom phần ngôn ngữ, mỗi child theme đều có thể tạo thêm ngôn ngữ riêng bằng cách mở comment function này ra
+/*
+function WGR_child_lang () {
+	global $___eb_lang;
+	global $eb_type_lang;
+	
+	$___eb_lang[eb_key_for_site_lang . 'new_key'] = 'new_key';
+	$eb_type_lang[eb_key_for_site_lang . 'new_type_key'] = 'number|textarea|default: text';
+}
+*/
+
+
